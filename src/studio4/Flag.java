beaee2ae9db3622a1104d88b4b7b92e3c9e357ba
Jack <Jack@DESKTOP-MTLJ4HC.dhcp.wustl.edu>
2022-10-13 14:03:32 -0500

    commit part 1

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,4 +1,4 @@
-package studio4;
+package studio4; 
 
 import java.awt.Color;
 
@@ -6,6 +6,17 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		System.out.println("asdfsa");
+		StdDraw.setPenColor(245,128,37);
+		StdDraw.filledRectangle(0.5, 0.5, 0.5, 0.3);
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledRectangle(0.5, 0.32, 0.5, 0.127);
+		StdDraw.setPenColor(Color.MAGENTA);
+		StdDraw.filledCircle(0.15, 0.65, 0.1);
+		StdDraw.setPenColor(Color.GREEN);
+		StdDraw.filledRectangle(0.75, 0.7, 0.15, 0.05);
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.setPenRadius(0.01);
+		StdDraw.text(0.75, 0.7, "$$$");
+		StdDraw.rectangle(0.5, 0.5, 0.495, 0.3);
 	}
 }
